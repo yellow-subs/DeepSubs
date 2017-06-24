f49a2169be6536d57d37c730dbf0d705cc951643
Phongtlam <Phongtlam@gmail.com>
2017-06-24 16:58:30 -0700

    before swapping to basic

--- client/src/components/Chessboard.jsx
+++ client/src/components/Chessboard.jsx
@@ -7,14 +7,36 @@ import { startNewGame } from '../redux/actions/index';
 import styles from '../styles/styles';
 
 
-const Chessboard = props => (
-  <Board
-    fen={props.boardState}
-    squareSize={styles.board.size}
-    lightSquareColor={styles.board.light}
-    darkSquareColor={styles.board.dark}
-  />
-);
+
+function onMovePiece(piece, fromSquare, toSquare) {
+	let message = 'You moved ' + piece + fromSquare + ' to ' + toSquare + ' !';
+	console.log(message);
+}
+const whitePos =
+  ['R@a1', 'N@b1', 'B@c1', 'Q@d1', 'K@e1', 'B@f1', 'N@g1', 'R@h1',
+    'P@a2', 'P@b2', 'P@c2', 'P@d2', 'P@e2', 'P@f2', 'P@g2', 'P@h2'];
+const blackPos =
+  ['r@a8', 'n@b8', 'b@c8', 'q@d8', 'k@e8', 'b@f8', 'n@g8', 'r@h8',
+    'p@a7', 'p@b7', 'p@c7', 'p@d7', 'p@e7', 'p@f7', 'p@g7', 'p@h7'];
+
+class Chessboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Board
+        fen={this.props.boardState}
+        pieces={whitePos.concat(blackPos)}
+        onMovePiece={onMovePiece}
+        squareSize={styles.board.size}
+        lightSquareColor={styles.board.light}
+        darkSquareColor={styles.board.dark}
+      />
+    );
+  }
+}
 
 const mapStateToProps = ({ board }) => {
   const { boardState } = board;
