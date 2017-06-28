b19cf517bb422285f848d170b9b8c0b7c5ecf311
Phongtlam <Phongtlam@gmail.com>
2017-06-27 20:33:40 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,18 @@ module.exports = {
   client: 'postgresql',
   connection: {
     database: 'deep_subs',
-    user: 'deeper',
+    user: 'postgres',
     password: 'valky1223',
     host: 'localhost',
     port: 5432,
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://qwkbtfjekorpaf:e64cb170751d11de363092ba23913dc127bca2f3a4e5a45677689d1992020731@ec2-23-21-220-48.compute-1.amazonaws.com:5432/ddbjt8j5mcbftq',
+      user: 'postgres',
+      password: 'valky1223',
+    },
   },
   migrations: {
     tableName: 'knex_migrations',
--- postgres_db/index.js
+++ postgres_db/index.js
@@ -1,17 +1,5 @@
 const CONFIG = require('../config/development');
 
-if (process.env.NODE_ENV = 'production') {
-  CONFIG.knex_config = {
-    client: 'postgresql',
-    connection: {
-      host: 'host',
-      user: 'deeper',
-      password: 'valky1223',
-      database: 'deep_subs',
-      charset: 'utf8',
-    },
-  };
-}
 const knex = require('knex')(CONFIG.knex_config);
 
 module.exports = knex;
