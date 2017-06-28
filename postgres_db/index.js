443cc2b026c63d0c584b7b8187b795a2adf2516c
Phongtlam <Phongtlam@gmail.com>
2017-06-27 19:58:31 -0700

    heroku

--- postgres_db/index.js
+++ postgres_db/index.js
@@ -1,4 +1,17 @@
 const CONFIG = require('../config/development');
+
+if (process.env.NODE_ENV = 'production') {
+  CONFIG.knex_config = {
+    client: 'postgresql',
+    connection: {
+      host: 'host',
+      user: 'deeper',
+      password: 'valky1223',
+      database: 'deep_subs',
+      charset: 'utf8',
+    },
+  };
+}
 const knex = require('knex')(CONFIG.knex_config);
 
 module.exports = knex;
